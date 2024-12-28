e4072919e62da365a211ed3bc89370e47e47b194
HarryLin46~ <r12942083@ntu.edu.tw>
2024-12-29 01:47:03 +0800

    Delete Version 3_2

--- version3_2/envs/__init__.py
+++
@@ -1,7 +0,0 @@
-#from envs.rook_modulize import RookEnv as RookEnv_v0
-from envs.rook import RookEnv as RookEnv_v1
-from envs.rook2 import RookEnv as RookEnv_v2
-from envs.rook3 import RookEnv as RookEnv_v3
-from envs.rook4 import RookEnv as RookEnv_v4
-from envs.rook_v5 import RookEnv as RookEnv_v5
-from envs.rook_v6 import RookEnv as RookEnv_v6
